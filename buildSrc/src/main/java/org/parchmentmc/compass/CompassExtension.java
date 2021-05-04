5ca86c59bf6545207f95b6f570c15f479668c018
SciWhiz12 <arnoldnunag12@gmail.com>
2021-05-04 16:17:52 +0800

    Add tasks to generate and promote staging data

--- buildSrc/src/main/java/org/parchmentmc/compass/CompassExtension.java
+++ buildSrc/src/main/java/org/parchmentmc/compass/CompassExtension.java
@@ -6,26 +6,33 @@ import org.gradle.api.provider.Property;
 
 public class CompassExtension {
     private final Property<String> manifestURL;
-    private final DirectoryProperty storageDirectory;
+    private final DirectoryProperty productionData;
     private final Property<String> version;
+    private final DirectoryProperty stagingData;
 
     public CompassExtension(final Project project) {
         manifestURL = project.getObjects().property(String.class)
                 .convention("https://launchermeta.mojang.com/mc/game/version_manifest_v2.json");
-        storageDirectory = project.getObjects().directoryProperty()
+        productionData = project.getObjects().directoryProperty()
                 .convention(project.getLayout().getProjectDirectory().dir("data"));
         version = project.getObjects().property(String.class);
+        stagingData = project.getObjects().directoryProperty()
+                .convention(project.getLayout().getProjectDirectory().dir("staging").dir("data"));
     }
 
     public Property<String> getLauncherManifestURL() {
         return manifestURL;
     }
 
-    public DirectoryProperty getStorageDirectory() {
-        return storageDirectory;
+    public DirectoryProperty getProductionData() {
+        return productionData;
     }
 
     public Property<String> getVersion() {
         return version;
+    }
+
+    public DirectoryProperty getStagingData() {
+        return stagingData;
     }
 }
--- buildSrc/src/main/java/org/parchmentmc/compass/CompassPlugin.java
+++ buildSrc/src/main/java/org/parchmentmc/compass/CompassPlugin.java
@@ -10,6 +10,7 @@ import org.gradle.api.logging.Logger;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.TaskProvider;
+import org.parchmentmc.compass.storage.MappingDataBuilder;
 import org.parchmentmc.compass.storage.MappingDataContainer;
 import org.parchmentmc.compass.storage.io.ExplodedDataIO;
 import org.parchmentmc.compass.tasks.DisplayMinecraftVersions;
@@ -18,8 +19,12 @@ import org.parchmentmc.compass.util.MappingUtil;
 import org.parchmentmc.compass.util.download.ManifestsDownloader;
 import org.parchmentmc.compass.util.download.ObfuscationMapsDownloader;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 
 public class CompassPlugin implements Plugin<Project> {
     public static final String COMPASS_GROUP = "compass";
@@ -44,13 +49,70 @@ public class CompassPlugin implements Plugin<Project> {
             t.getManifest().set(manifests.getLauncherManifest());
         });
 
+        TaskProvider<DefaultTask> generateVersionBase = tasks.register("generateVersionBase", DefaultTask.class);
+        generateVersionBase.configure(t -> {
+            t.setGroup(COMPASS_GROUP);
+            t.setDescription("Generates the base data for the active version to the staging directory.");
+            t.doLast(_t -> {
+                IMappingFile obfMap = obfuscationMaps.getObfuscationMap().get();
+                // reversed because normally, obf map is [Moj -> Obf] (because it's a ProGuard log of the obf)
+                MappingDataBuilder data = MappingUtil.constructPackageData(MappingUtil.createBuilderFrom(obfMap, true));
+
+                File stagingDataDir = extension.getStagingData().get().getAsFile();
+
+                try {
+                    ExplodedDataIO.INSTANCE.write(data, stagingDataDir);
+                } catch (IOException e) {
+                    throw new UncheckedIOException("Failed to write base data for active version to staging directory", e);
+                }
+            });
+        });
+
+        TaskProvider<DefaultTask> promoteStagingToProduction = tasks.register("promoteStagingToProduction", DefaultTask.class);
+        promoteStagingToProduction.configure(t -> {
+            t.setGroup(COMPASS_GROUP);
+            t.setDescription("Promotes the staging data to production.");
+            t.doLast(_t -> {
+                File stagingDataDir = extension.getStagingData().get().getAsFile();
+                if (stagingDataDir.exists()) {
+                    String[] list = stagingDataDir.list();
+                    if (list != null && list.length == 0) return;
+                } else {
+                    return;
+                }
+
+                MappingDataContainer staging;
+                try {
+                    staging = ExplodedDataIO.INSTANCE.read(stagingDataDir.toPath());
+                } catch (IOException e) {
+                    throw new UncheckedIOException("Failed to read staging data for promotion", e);
+                }
+                try {
+                    // noinspection ResultOfMethodCallIgnored
+                    Files.walk(stagingDataDir.toPath())
+                            .sorted(Comparator.reverseOrder())
+                            .map(Path::toFile)
+                            .forEach(File::delete);
+                } catch (IOException e) {
+                    t.getLogger().warn("Unable to delete staging data directory; continuing", e);
+                }
+
+                File prodDataDir = extension.getProductionData().get().getAsFile();
+                try {
+                    ExplodedDataIO.INSTANCE.write(staging, prodDataDir.toPath());
+                } catch (IOException e) {
+                    throw new UncheckedIOException("Failed to write promoted staging->production data", e);
+                }
+            });
+        });
+
         DefaultTask writeExploded = tasks.create("writeExploded", DefaultTask.class);
         writeExploded.setGroup(COMPASS_GROUP);
         writeExploded.setDescription("temporary task; Writes out the combined obfuscation maps into exploded directories.");
         writeExploded.doLast(t -> {
             Provider<IMappingFile> obfMapProvider = obfuscationMaps.getObfuscationMap();
             try {
-                Path output = extension.getStorageDirectory().get().getAsFile().toPath();
+                Path output = extension.getProductionData().get().getAsFile().toPath();
                 IMappingFile mojToObf = obfMapProvider.get();
 
                 // getMapped() == obfuscated, getOriginal() == mojmap
