e918458d63f5b7e7051a447e2666842828898433
SciWhiz12 <arnoldnunag12@gmail.com>
2021-05-15 05:26:28 +0800

    Remove invalid imports leftover from prev. commit

--- buildSrc/src/main/java/org/parchmentmc/compass/package-info.java
+++ buildSrc/src/main/java/org/parchmentmc/compass/package-info.java
@@ -6,6 +6,3 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.common.subtyping.qual.Unqualified;
 import org.checkerframework.framework.qual.DefaultQualifier;
 import org.checkerframework.framework.qual.TypeUseLocation;
-import org.parchmentmc.compass.util.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
