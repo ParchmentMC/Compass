76b74e5b32b409931478b4a58b1cde6006a4b9b9
sciwhiz12 <arnoldnunag12@gmail.com>
2021-09-29 23:21:40 +0800

    Add sanitizer to move data from bouncer to targets

---
+++ src/main/java/org/parchmentmc/compass/sanitation/impl/BouncerDataMover.java
@@ -0,0 +1,66 @@
+package org.parchmentmc.compass.sanitation.impl;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.parchmentmc.compass.sanitation.AbstractSanitizer;
+import org.parchmentmc.feather.metadata.BouncingTargetMetadata;
+import org.parchmentmc.feather.metadata.ClassMetadata;
+import org.parchmentmc.feather.metadata.MethodMetadata;
+import org.parchmentmc.feather.metadata.MethodReference;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.parchmentmc.feather.mapping.MappingDataContainer.*;
+
+/**
+ * Moves data in bouncer methods to their targets.
+ */
+public class BouncerDataMover extends AbstractSanitizer {
+    // First pass is to collect bouncers and delete them, second pass is applying the data from bouncers to their targets
+    private boolean finishedCollecting = false;
+    private final Map<String, MethodData> data = new HashMap<>();
+
+    public BouncerDataMover() {
+        super("bouncer data mover");
+    }
+
+    @Override
+    public Action<MethodData> sanitize(ClassData classData, MethodData methodData,
+                                       @Nullable ClassMetadata classMetadata, @Nullable MethodMetadata methodMetadata) {
+        if (!finishedCollecting && methodMetadata != null && methodMetadata.getBouncingTarget().isPresent()) {
+            final BouncingTargetMetadata targetData = methodMetadata.getBouncingTarget().get();
+            if (targetData.getTarget().isPresent()) {
+                final MethodReference targetRef = targetData.getTarget().get();
+
+                String targetRefString = targetRef.getOwner().getMojangName("") + "#"
+                    + targetRef.getName().getMojangName("") + "#"
+                    + targetRef.getDescriptor().getMojangName("");
+
+                data.put(targetRefString, methodData);
+                return Action.delete();
+            }
+        }
+
+        if (finishedCollecting) {
+            String methodRefString = classData.getName() + "#"
+                + methodData.getName() + "#" + methodData.getDescriptor();
+
+            final MethodData bouncerData = data.remove(methodRefString);
+            if (bouncerData != null) {
+                return Action.modify(bouncerData, true);
+            }
+        }
+
+        return Action.skip();
+    }
+
+    @Override
+    public boolean revisit() {
+        if (finishedCollecting) {
+            data.clear();
+            return false;
+        }
+        finishedCollecting = true;
+        return true;
+    }
+}
