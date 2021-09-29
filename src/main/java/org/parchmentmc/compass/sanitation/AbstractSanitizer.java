ab221dcb8c6953a582da30e3cbb0c8da5f9eb91e
sciwhiz12 <arnoldnunag12@gmail.com>
2021-09-29 21:51:44 +0800

    Add the skeleton of a sanitizer structure
    The goal is to create a modular structure for sanitization akin to the
    current Validator system.

---
+++ src/main/java/org/parchmentmc/compass/sanitation/AbstractSanitizer.java
@@ -0,0 +1,20 @@
+package org.parchmentmc.compass.sanitation;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * An abstract base class for sanitizers, for extending by implementors.
+ */
+public abstract class AbstractSanitizer implements Sanitizer {
+    private final String name;
+
+    protected AbstractSanitizer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    @NonNull
+    public final String getName() {
+        return name;
+    }
+}
