67c44f1b1e3bd8007bb2053e5fb64c3cfcdfc23b
sciwhiz12 <arnoldnunag12@gmail.com>
2021-10-07 20:16:37 +0800

    Change SanitizeData task to use Sanitizer

--- src/main/java/org/parchmentmc/compass/tasks/SanitizeData.java
+++ src/main/java/org/parchmentmc/compass/tasks/SanitizeData.java
@@ -1,14 +1,17 @@
 package org.parchmentmc.compass.tasks;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.NamedDomainObjectList;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.logging.Logger;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.TaskAction;
 import org.parchmentmc.compass.CompassPlugin;
 import org.parchmentmc.compass.sanitation.DataSanitizer;
+import org.parchmentmc.compass.sanitation.Sanitizer;
 import org.parchmentmc.compass.sanitation.impl.BouncerDataMover;
 import org.parchmentmc.compass.sanitation.impl.DescriptorParametersSanitizer;
 import org.parchmentmc.compass.sanitation.impl.EnumValueOfRemover;
@@ -18,15 +21,33 @@ import org.parchmentmc.compass.util.download.BlackstoneDownloader;
 import org.parchmentmc.feather.mapping.MappingDataContainer;
 import org.parchmentmc.feather.metadata.SourceMetadata;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 
 public abstract class SanitizeData extends DefaultTask {
+    private final NamedDomainObjectList<Sanitizer> sanitizers;
+
     @InputDirectory
     public abstract DirectoryProperty getInput();
 
     @Input
     public abstract Property<MappingIOFormat> getInputFormat();
+
+    @Input
+    public NamedDomainObjectList<Sanitizer> getSanitizers() {
+        return sanitizers;
+    }
+
+    @Inject
+    public SanitizeData(ObjectFactory objectFactory) {
+        sanitizers = objectFactory.namedDomainObjectList(Sanitizer.class);
+
+        getSanitizers().add(new BouncerDataMover());
+        getSanitizers().add(new EnumValueOfRemover());
+        getSanitizers().add(new SyntheticsRemover());
+        getSanitizers().add(new DescriptorParametersSanitizer());
+    }
 
     @TaskAction
     public void sanitize() throws IOException {
@@ -45,10 +66,8 @@ public abstract class SanitizeData extends DefaultTask {
 
         DataSanitizer sanitizer = new DataSanitizer();
 
-        sanitizer.addSanitizer(new BouncerDataMover());
-        sanitizer.addSanitizer(new EnumValueOfRemover());
-        sanitizer.addSanitizer(new SyntheticsRemover());
-        sanitizer.addSanitizer(new DescriptorParametersSanitizer());
+        getSanitizers().forEach(sanitizer::addSanitizer);
+        sanitizer.getSanitizers().forEach(s -> System.out.println(s.getName()));
 
         final MappingDataContainer sanitizedData = sanitizer.sanitize(inputData, metadata);
 
