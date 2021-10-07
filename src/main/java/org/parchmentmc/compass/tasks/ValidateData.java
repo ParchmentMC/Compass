d4a96af75c36c056a133255e8eac1b69bc965ce8
sciwhiz12 <arnoldnunag12@gmail.com>
2021-10-07 22:06:35 +0800

    Change ValidateData to use a named domain list
    This allows customizing in buildscripts the validators in use, instead
    of being hardcoded in the task action.

--- src/main/java/org/parchmentmc/compass/tasks/ValidateData.java
+++ src/main/java/org/parchmentmc/compass/tasks/ValidateData.java
@@ -2,25 +2,29 @@ package org.parchmentmc.compass.tasks;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Named;
+import org.gradle.api.NamedDomainObjectList;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.logging.Logger;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.VerificationTask;
 import org.parchmentmc.compass.CompassPlugin;
+import org.parchmentmc.compass.data.validation.ValidationIssue;
+import org.parchmentmc.compass.data.validation.Validator;
+import org.parchmentmc.compass.data.validation.action.DataValidator;
+import org.parchmentmc.compass.data.validation.impl.*;
 import org.parchmentmc.compass.storage.io.MappingIOFormat;
 import org.parchmentmc.compass.util.ResultContainer;
 import org.parchmentmc.compass.util.download.BlackstoneDownloader;
-import org.parchmentmc.compass.data.validation.ValidationIssue;
-import org.parchmentmc.compass.data.validation.action.DataValidator;
-import org.parchmentmc.compass.data.validation.impl.*;
 import org.parchmentmc.feather.mapping.MappingDataContainer;
 import org.parchmentmc.feather.metadata.SourceMetadata;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -29,6 +33,7 @@ import java.util.stream.Collectors;
 public abstract class ValidateData extends DefaultTask implements VerificationTask {
     private static final Marker VALIDATION = MarkerFactory.getMarker("compass/task/validation");
 
+    private final NamedDomainObjectList<Validator> validators;
     private boolean ignoreFailures = false;
 
     @InputDirectory
@@ -36,6 +41,26 @@ public abstract class ValidateData extends DefaultTask implements VerificationTa
 
     @Input
     public abstract Property<MappingIOFormat> getInputFormat();
+
+    @Input
+    public NamedDomainObjectList<Validator> getValidators() {
+        return validators;
+    }
+
+    @Inject
+    public ValidateData(ObjectFactory objectFactory) {
+        validators = objectFactory.namedDomainObjectList(Validator.class);
+
+        validators.add(new BridgeValidator());
+        validators.add(new ClassInitValidator());
+        validators.add(new EnumValidator());
+        validators.add(new LambdaValidator());
+        validators.add(new ParameterStandardsValidator());
+        validators.add(new SyntheticValidator());
+        validators.add(new MethodStandardsValidator());
+        validators.add(new ParameterConflictsValidator());
+        validators.add(new ParameterIndexValidator());
+    }
 
     @TaskAction
     public void validate() throws IOException {
@@ -49,15 +74,7 @@ public abstract class ValidateData extends DefaultTask implements VerificationTa
         MappingDataContainer data = getInputFormat().get().read(input);
 
         final DataValidator validator = new DataValidator();
-        validator.addValidator(new BridgeValidator());
-        validator.addValidator(new ClassInitValidator());
-        validator.addValidator(new EnumValidator());
-        validator.addValidator(new LambdaValidator());
-        validator.addValidator(new ParameterStandardsValidator());
-        validator.addValidator(new SyntheticValidator());
-        validator.addValidator(new MethodStandardsValidator());
-        validator.addValidator(new ParameterConflictsValidator());
-        validator.addValidator(new ParameterIndexValidator());
+        getValidators().forEach(validator::addValidator);
 
         final Logger logger = getProject().getLogger();
 
